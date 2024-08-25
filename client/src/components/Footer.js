684da29a22f51ff26cde0cb40d2e7cc7eea752b4
sujallchavan <sujallchavan@gmail.com>
2024-08-25 21:23:41 +0530

    given some names to the gallary and slider

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -50,7 +50,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-bottom">
-        <p>&copy; 2024 Company Name. All rights reserved.</p>
+        <p>&copy; 2024 ChavanIndustrialWorks. All rights reserved.</p>
       </div>
     </footer>
   );
